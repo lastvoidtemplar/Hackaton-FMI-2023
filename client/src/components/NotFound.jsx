e76f0880f71529666dbd51ed44921f3ddac593be
Peshka564 <matpet0412@gmail.com>
2023-03-18 21:54:13 +0200

    Cosmetic changes2

--- client/src/components/NotFound.jsx
+++ client/src/components/NotFound.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <h1 className='text-white fs-1 mx-auto mt-5'>404 Not Found</h1>
+    <h1 className='text-white fs-1'>404 Not Found</h1>
   )
 }
 
