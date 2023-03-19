10e96ab40c58ef7ad6f14b73207d51c88523a5f1
Peshka564 <matpet0412@gmail.com>
2023-03-19 06:17:41 +0200

    Clean up client

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -9,22 +9,12 @@ import NotFound from './components/NotFound'
 
 const MainRoutes = () => {
 
-
-
-    /*
-    Проблемът май е решен
-    env променливата FRONTEND_REDIRECT_URL беше написана с localhost а не с 127.0.0.1
-    */
-
-
-
-
     return (
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/home" element={<AuthGuard component={Home} />}/>
         <Route path="/callback" element={<Callback />}/>
-        <Route path="/party/:code" element={<Party />} />
+        <Route path="/party/:code" element={<AuthGuard component={Party}/>} />
         <Route path="*" element={<NotFound />} />
         {/* "/" -> <Home />
             "/vote" -> <VotingPoll> 
--- client/src/components/AuthGuard.jsx
+++ client/src/components/AuthGuard.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { withAuthenticationRequired } from '@auth0/auth0-react'
 import Callback from '../Callback';
-import Header from './Header';
 
 export const AuthGuard = ({ component }) => {
     const Component = withAuthenticationRequired(component, {
