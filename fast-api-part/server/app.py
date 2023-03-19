0a68c36111ddf86b3cea627ba6040d13e02ba6fd
Sergey Dankoglo <sdankoglo2310@gmail.com>
2023-03-19 10:25:17 +0200

    Added comments

--- fast-api-part/server/app.py
+++ fast-api-part/server/app.py
@@ -16,7 +16,9 @@ async def join_to_room(
                        code: int = Body(), 
                        token: str = Depends(token_auth_scheme)):
 
+    # Verify AUTH0 JWT token
     verification_info = verify_token(token.credentials)
+    # Add provided user to the party
     if verification_info.get('status'):
             
             document = await find_one_item({'code': code})
@@ -61,7 +63,6 @@ async def leave_room(
     
     why = verification_info.get('msg')
     raise HTTPException(status_code=400, detail=f"Your token isn't valid: {why}")
-
 
 async def get_qr_code(response: Response,
                 user_id: str = Body(),
