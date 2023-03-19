43f18f5ba1eddaa53048ddff40a411a69931e911
Deyan Delchev <60808936+lastvoidtemplar@users.noreply.github.com>
2023-03-19 13:36:15 +0200

    docker

--- fast-api-part/main.py
+++ fast-api-part/main.py
@@ -2,4 +2,4 @@ import uvicorn
 
 if __name__ == '__main__':
     # Run development server
-    uvicorn.run("server.app:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("server.app:app", host="0.0.0.0", port=8000, reload=True)
